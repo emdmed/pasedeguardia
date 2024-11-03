5a153134cfc74b73d71db146f5704f883e76b967
emdmed <enrique.darderes@gmail.com>
2024-11-03 20:42:38 +0100

    new qr code reader

--- src/app/components/import/qrCodeImporter.js
+++ src/app/components/import/qrCodeImporter.js
@@ -1,12 +1,9 @@
 import { useState, useEffect } from 'react';
-import dynamic from 'next/dynamic';
-import { Button } from '@/components/ui/button';
+import { Scanner } from '@yudiel/react-qr-scanner';
 
-const QrScanner = dynamic(() => import('react-qr-scanner'), { ssr: false });
-
-function QRCodeImporter({ setToggleShareDialog }) {
+function QRCodeImporter({ onDataComplete }) {
   const [cameraAvailable, setCameraAvailable] = useState(false);
-  const [scanning, setScanning] = useState(false); // New state for scanning feedback
+  const [scanning, setScanning] = useState(false); // State for scanning feedback
   const [scanResult, setScanResult] = useState([]);
   const [currentPart, setCurrentPart] = useState(1);
 
@@ -25,17 +22,25 @@ function QRCodeImporter({ setToggleShareDialog }) {
     checkCamera();
   }, []);
 
-  const handleFinish = () => {
-    const completeData = scanResult.join("")
-    localStorage.setItem("patients", completeData)
-    setToggleShareDialog(false)
-  }
-
   const handleScan = (data) => {
-    if (data && scanning) {
+    if (data) {
       console.log("QR code detected:", data); // Log the raw data from QR code
       setScanning(false); // Stop scanning once data is detected
-      setScanResult([...scanResult, data.text])
+
+      const [header, content] = data.split(':');
+      const [_, part, total] = header.match(/part (\d+)\/(\d+)/) || [];
+      const parsedPart = parseInt(part, 10);
+      const parsedTotal = parseInt(total, 10);
+
+      if (parsedPart === currentPart && content) {
+        setScanResult(prev => [...prev, content]);
+        setCurrentPart(currentPart + 1);
+
+        if (scanResult.length + 1 === parsedTotal) {
+          const fullData = scanResult.join('') + content;
+          onDataComplete(fullData);
+        }
+      }
     }
   };
 
@@ -56,16 +61,13 @@ function QRCodeImporter({ setToggleShareDialog }) {
       ) : (
         <p style={{ color: "red" }}>QR code detected, processing...</p>
       )}
-      <QrScanner
-        delay={300}
+      <Scanner
+        onDecode={handleScan}
         onError={handleError}
-        onScan={handleScan}
+        facingMode="environment" // Use back camera if available
         style={{ width: '100%' }}
-        facingMode="rear" // Use back camera if available
       />
       <p>Parts scanned: {scanResult.length}</p>
-      <Button disabled={scanning} onClick={e => setScanning(true)}>Proximo</Button>
-      <Button onClick={handleFinish}>Finalizar</Button>
     </div>
   );
 }
